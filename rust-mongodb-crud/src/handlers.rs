466c500b5d4e209cd726674d9984a9541fe6be22
RAprogramm <andrey.rozanov.vl@gmail.com>
2023-12-31 08:56:15 +0900

    add: handlers

---
+++ rust-mongodb-crud/src/handlers.rs
@@ -0,0 +1,158 @@
+use crate::{
+    db::DB,
+    response::GenericResponse,
+    schema::UpdateNoteSchema,
+    schema::{CreateNoteSchema, FilterOptions},
+    WebResult,
+};
+use warp::{http::StatusCode, reject, reply::json, reply::with_status, Reply};
+
+/// Handles the retrieval of a list of notes based on the provided options.
+///
+/// # Arguments
+///
+/// * `opts` - FilterOptions containing parameters like page number and limit.
+/// * `db` - An instance of the DB struct, representing the database connection.
+///
+/// # Returns
+///
+/// Returns a Warp Result containing the JSON representation of the NoteListResponse or a rejection if an error occurs.
+pub async fn notes_list_handler(opts: FilterOptions, db: DB) -> Result<impl Reply, Rejection> {
+    // Extract limit and page from FilterOptions or use default values if not provided
+    let limit = opts.limit.unwrap_or(10) as i64;
+    let page = opts.page.unwrap_or(1) as i64;
+
+    // Fetch notes from the database based on provided options
+    let result_json = db
+        .fetch_notes(limit, page)
+        .await
+        .map_err(|e| reject::custom(e))?; // Map errors to a custom rejection
+
+    // Return the JSON representation of the fetched notes
+    Ok(reply::json(&result_json))
+}
+
+/// Handles the creation of a new note based on the provided schema.
+///
+/// # Arguments
+///
+/// * `body` - CreateNoteSchema containing details of the note to be created.
+/// * `db` - An instance of the DB struct, representing the database connection.
+///
+/// # Returns
+///
+/// Returns a Warp Result containing the JSON representation of the created note or a rejection if an error occurs.
+pub async fn create_note_handler(body: CreateNoteSchema, db: DB) -> Result<impl Reply, Rejection> {
+    // Create a new note based on the provided schema
+    let note = db.create_note(&body).await.map_err(|e| reject::custom(e))?;
+
+    // Return the JSON representation of the created note with a status code indicating successful creation
+    Ok(with_status(reply::json(&note), StatusCode::CREATED))
+}
+
+/// Handles retrieval of a specific note based on the provided ID.
+///
+/// # Arguments
+///
+/// * `id` - String representing the ID of the note to retrieve.
+/// * `db` - An instance of the DB struct, representing the database connection.
+///
+/// # Returns
+///
+/// Returns a Warp Result containing the JSON representation of the requested note if found,
+/// or a JSON response with a 'not found' status if the note does not exist, or a rejection if an error occurs.
+pub async fn get_note_handler(id: String, db: DB) -> Result<impl Reply, Rejection> {
+    // Retrieve the note based on the provided ID
+    let note = db.get_note(&id).await.map_err(|e| reject::custom(e))?;
+
+    // Construct an error response if the note is not found
+    let error_response = GenericResponse {
+        status: "fail".to_string(),
+        message: format!("Note with ID: {} not found", id),
+    };
+
+    // Check if the note exists and return the appropriate response
+    if note.is_none() {
+        return Ok(with_status(
+            reply::json(&error_response),
+            StatusCode::NOT_FOUND,
+        ));
+    }
+
+    // Return the JSON representation of the retrieved note with a success status
+    Ok(with_status(reply::json(&note), StatusCode::OK))
+}
+
+/// Handles editing an existing note based on the provided ID and request body.
+///
+/// # Arguments
+///
+/// * `id` - String representing the ID of the note to edit.
+/// * `body` - An instance of UpdateNoteSchema containing the updated note details.
+/// * `db` - An instance of the DB struct, representing the database connection.
+///
+/// # Returns
+///
+/// Returns a Warp Result containing the JSON representation of the edited note if successful,
+/// or a JSON response with a 'not found' status if the note does not exist, or a rejection if an error occurs.
+pub async fn edit_note_handler(
+    id: String,
+    body: UpdateNoteSchema,
+    db: DB,
+) -> Result<impl Reply, Rejection> {
+    // Edit the note based on the provided ID and request body
+    let note = db
+        .edit_note(&id, &body)
+        .await
+        .map_err(|e| reject::custom(e))?;
+
+    // Construct an error response if the note is not found
+    let error_response = GenericResponse {
+        status: "fail".to_string(),
+        message: format!("Note with ID: {} not found", id),
+    };
+
+    // Check if the note exists and return the appropriate response
+    if note.is_none() {
+        return Ok(with_status(
+            reply::json(&error_response),
+            StatusCode::NOT_FOUND,
+        ));
+    }
+
+    // Return the JSON representation of the edited note with a success status
+    Ok(with_status(reply::json(&note), StatusCode::OK))
+}
+
+/// Handles the deletion of a note based on the provided ID.
+///
+/// # Arguments
+///
+/// * `id` - String representing the ID of the note to delete.
+/// * `db` - An instance of the DB struct, representing the database connection.
+///
+/// # Returns
+///
+/// Returns a Warp Result containing a 'no content' response if the note is successfully deleted,
+/// or a JSON response with a 'not found' status if the note does not exist, or a rejection if an error occurs.
+pub async fn delete_note_handler(id: String, db: DB) -> Result<impl Reply, Rejection> {
+    // Delete the note based on the provided ID
+    let result = db.delete_note(&id).await.map_err(|e| reject::custom(e))?;
+
+    // Construct an error response if the note is not found
+    let error_response = GenericResponse {
+        status: "fail".to_string(),
+        message: format!("Note with ID: {} not found", id),
+    };
+
+    // Check if the note exists and return the appropriate response
+    if result.is_none() {
+        return Ok(with_status(
+            reply::json(&error_response),
+            StatusCode::NOT_FOUND,
+        ));
+    }
+
+    // Return a 'no content' response indicating successful deletion
+    Ok(with_status(reply::json(&""), StatusCode::NO_CONTENT))
+}
